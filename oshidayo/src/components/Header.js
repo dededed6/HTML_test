85ef767761ad3fc7962eb5995d59c379374cd824
dededed6 <48098274+dededed6@users.noreply.github.com>
2024-10-12 15:02:39 +0000

    animation

--- oshidayo/src/components/Header.js
+++ oshidayo/src/components/Header.js
@@ -2,16 +2,17 @@ import '../styles/components/Header.css'
 import { Link } from "react-router-dom";
 
 export default function Header() {
+
+
     return (
-        <div>
-            <p className='header'>
-                <Link
-                    to={'/'}
-                    style={{ textDecoration: "none", color: 'inherit' }}
-                >
-                    오시다요
-                </Link>
-            </p>
-        </div>
+        <header>
+            <Link className='headerLink' to={'/'}>
+                <p className='headerTitle oshi'>오시</p>
+                <p className='headerTitle yo'>요</p>
+                <p className='headerTitle gura'>구라</p>
+                <p className='headerTitle marine'>마린</p>
+                <p className='headerTitle dayo'>다요</p>
+            </Link>
+        </header>
     );
 }
